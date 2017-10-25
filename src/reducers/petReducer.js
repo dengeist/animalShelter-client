9b34d283cffcc0046a53b8daca960384d7eaa7b2
Elias Mason <eliasjmason@gmail.com>
2017-10-25 00:56:34 -0700

    Remove static data from reducer, handle actions with object assign

--- src/reducers/petReducer.js
+++ src/reducers/petReducer.js
@@ -1,38 +1,23 @@
 import { FETCH_CAT_SUCCESS } from '../actions/catAction';
 import { FETCH_DOG_SUCCESS } from '../actions/dogAction';
 
-const catToAdopt = {
-  imageURL:
-    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-  name: 'Fluffy',
-  gender: 'Female',
-  age: '2 yrs',
-  breed: 'Bengal',
-  story: 'thrown on the street'
-};
-const dogToAdopt = {
-  imageURL:
-    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-  name: 'June',
-  gender: 'Female',
-  age: '1 yrs',
-  breed: 'Shiba',
-  story: 'Rejected by mother'
-};
-
 const initialState = {
-  cat: catToAdopt,
-  dog: dogToAdopt
+  cat: {},
+  dog: {}
 };
 
 export default function petReducer(state=initialState, action) {
 
   if (action.type === FETCH_CAT_SUCCESS) {
-    return action.cat;
+    return Object.assign({}, state, {
+      cat: action.cat
+    })
   }
   
   if (action.type === FETCH_DOG_SUCCESS) {
-    return action.dog;
+    return Object.assign({}, state, {
+      dog: action.dog
+    })
   }
   
   return state;
