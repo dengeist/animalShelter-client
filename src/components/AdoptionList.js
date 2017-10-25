f1c52e2336dbcaadb17b441cc5af69ad91d7d520
Elias Mason <eliasjmason@gmail.com>
2017-10-24 22:16:45 -0700

    DRY up animal render

--- src/components/AdoptionList.js
+++ src/components/AdoptionList.js
@@ -1,26 +1,35 @@
 import React from 'react';
 
 export default function AdoptAPet(props) {
-  const {catToAdopt, dogToAdopt} = props;
-  const [ catKeys, dogKeys ] = [ Object.keys(catToAdopt), Object.keys(dogToAdopt)]
+  const { catToAdopt, dogToAdopt } = props;
+  const [catKeys, dogKeys] = [Object.keys(catToAdopt), Object.keys(dogToAdopt)];
+  const [catList, dogList] = [
+    catKeys.map(generateHTML, catToAdopt),
+    dogKeys.map(generateHTML, dogToAdopt)
+  ];
 
-  const catList = catKeys.map(
-    (prop, idx) => 
-    <li data-prop={prop}>{catToAdopt[prop]}</li>
-  );
-  const dogList = dogKeys.map(
-    (prop, idx) => 
-    <li data-prop={prop}>{dogToAdopt[prop]}</li>
-  );
+  function generateHTML(prop, idx) {
+    const keyHash = prop + '-' + idx;
+
+    let itemElem = (
+      <li data-prop={prop} key={keyHash}>
+        {this[prop]}
+      </li>
+    );
+    if (prop === 'imageURL') {
+      itemElem = (
+        <li data-prop="image" key={keyHash}>
+          <img src={this[prop]} alt=""/>
+        </li>
+      );
+    }
+    return itemElem;
+  }
 
   return (
     <div className="animals">
-      <ul data-animal="cat">
-        {catList}
-      </ul>
-      <ul data-animal="dog">
-        {dogList}
-      </ul>
+      <ul data-animal="cat">{catList}</ul>
+      <ul data-animal="dog">{dogList}</ul>
     </div>
   );
 }
