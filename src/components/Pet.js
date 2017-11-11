1783e21c76a868646a0c5981b9640ada72ee2ba6
Elias Mason <eliasjmason@gmail.com>
2017-11-11 11:31:19 -0800

    Refactor to use dl; beautify

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
-export default function Pet(props){
+export default function Pet(props) {
   return (
-    <section>
-    <header>
-      <h2 data-prop="name">{props.name}</h2>
-      <img src={props.imageURL} alt="" />
-    </header>
-    <main>
-      <h3>More about {props.name}</h3>
-      <ul>
-        <li data-prop="gender">{props.gender}</li>
-        <li data-prop="age">{props.age}</li>
-        <li data-prop="breed">{props.breed}</li>
-        <li data-prop="story">{props.story}</li>
-      </ul>
-      <button 
-        type="button"
-        onClick={() => props.onAdoptPet()}
-      >
-        Adopt
-      </button>
-    </main>
-  </section>
+    <section className="animal">
+      <header>
+        <h2 data-prop="name">{props.name}</h2>
+        <img src={props.imageURL} alt="" />
+      </header>
+      <main>
+        <h3>More about {props.name}</h3>
+        <dl>
+          <dt>Sex</dt>
+          <dd>{props.gender}</dd>
+          <dt>Age</dt>
+          <dd>{props.age}</dd>
+          <dt>Breed</dt>
+          <dd>{props.breed}</dd>
+          <dt>Story</dt>
+          <dd>{props.story}</dd>
+        </dl>
+        <button type="button" onClick={() => props.onAdoptPet()}>
+          Adopt
+        </button>
+      </main>
+    </section>
   );
 }
