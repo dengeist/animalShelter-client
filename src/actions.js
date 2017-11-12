1d22da263372f8332c7ec51653e3713fa092ed31
Elias Mason <eliasjmason@gmail.com>
2017-11-11 17:45:27 -0800

    Reorganize

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,30 @@
 import { REACT_APP_API_BASE_URL } from './config';
 
-export const ADOPT_PET_SUCCESS = 'ADOPT_PET_SUCCESS';
-export const adoptPetSuccess = species => ({
+/**
+ * Makes a GET requerst to show an animal in shelter.
+ * @param {string} species - The animal to look at. Either 'cat' or 'dog'.
+ */
+export const fetchPet = (species) => dispatch => {
+  console.log(`Attempting to fetch a ${species}`);
+  fetch(`${REACT_APP_API_BASE_URL}/${species}`)
+    .then(res => {
+      if (!res.ok) {
+        console.log(`error fetching the ${species}!`);
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(pet => {
+      console.log(`Got a ${species}! Dispatching fetchPetSuccess`);
+      dispatch(fetchPetSuccess(pet, species));
+    });
+};
+
+export const FETCH_PET_SUCCESS = 'FETCH_PET_SUCCESS';
+export const fetchPetSuccess = (pet, species) => ({
+  pet,
   species,
-  type: ADOPT_PET_SUCCESS
+  type: FETCH_PET_SUCCESS
 });
 
 /**
@@ -30,31 +51,8 @@ export const adoptPet = species => dispatch => {
   );
 };
 
-
-export const FETCH_PET_SUCCESS = 'FETCH_PET_SUCCESS';
-export const fetchPetSuccess = (pet, species) => ({
-  pet,
+export const ADOPT_PET_SUCCESS = 'ADOPT_PET_SUCCESS';
+export const adoptPetSuccess = species => ({
   species,
-  type: FETCH_PET_SUCCESS
+  type: ADOPT_PET_SUCCESS
 });
-
-
-/**
- * Makes a GET requerst to show an animal in shelter.
- * @param {string} species - The animal to look at. Either 'cat' or 'dog'.
- */
-export const fetchPet = (species) => dispatch => {
-  console.log(`Attempting to fetch a ${species}`);
-  fetch(`${REACT_APP_API_BASE_URL}/${species}`)
-    .then(res => {
-      if (!res.ok) {
-        console.log(`error fetching the ${species}!`);
-        return Promise.reject(res.statusText);
-      }
-      return res.json();
-    })
-    .then(pet => {
-      console.log(`Got a ${species}! Dispatching fetchPetSuccess`);
-      dispatch(fetchPetSuccess(pet, species));
-    });
-};
