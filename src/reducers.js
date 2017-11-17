b1c137e07d4b6f653a6a6d2980b153ad7f84d443
Elias Mason <eliasjmason@gmail.com>
2017-11-17 15:16:38 -0800

    Add new actions to reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,11 @@
-import { ADOPT_PET_SUCCESS, FETCH_PET_SUCCESS } from './actions';
+import {
+  ADOPT_PET_REQUEST,
+  ADOPT_PET_SUCCESS,
+  ADOPT_PET_ERROR,
+  FETCH_PET_REQUEST,
+  FETCH_PET_ERROR,
+  FETCH_PET_SUCCESS
+} from './actions';
 
 const initialState = {
   cat: {
@@ -14,14 +21,45 @@ const initialState = {
 };
 
 export default function reducers(state = initialState, action) {
-  if (action.type === ADOPT_PET_SUCCESS) {
+  if (action.type === ADOPT_PET_REQUEST) {
     return Object.assign({}, state, {
-      [action.species]: {data: null}
+      [action.species]: {
+        error: null,
+        loading: true
+      }
     });
   }
-  if (action.type === FETCH_PET_SUCCESS) {
+
+  if (action.type === ADOPT_PET_ERROR) {
     return Object.assign({}, state, {
-      [action.species]: {data: action.pet}
+      [action.species]: {
+        error: action.error,
+        loading: false
+      }
+    });
+  }
+
+  if (action.type === ADOPT_PET_SUCCESS) {
+    return Object.assign({}, state, {
+      [action.species]: { data: null }
+    });
+  }
+
+  if (action.type === FETCH_PET_REQUEST) {
+    return Object.assign({}, state, {
+      [action.species]: {
+        error: null,
+        loading: true
+      }
+    });
+  }
+
+  if (action.type === FETCH_PET_ERROR) {
+    return Object.assign({}, state, {
+      [action.species]: {
+        error: action.error,
+        loading: false
+      }
     });
   }
 
