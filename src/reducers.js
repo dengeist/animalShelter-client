fa70abb685536f252229b1cdc56c072cd1bce1af
Elias Mason <eliasjmason@gmail.com>
2017-11-17 14:53:38 -0800

    Restructure reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,19 +1,27 @@
 import { ADOPT_PET_SUCCESS, FETCH_PET_SUCCESS } from './actions';
 
 const initialState = {
-  cat: null,
-  dog: null
+  cat: {
+    data: null,
+    error: null,
+    loading: false
+  },
+  dog: {
+    data: null,
+    error: null,
+    loading: false
+  }
 };
 
 export default function reducers(state = initialState, action) {
   if (action.type === ADOPT_PET_SUCCESS) {
     return Object.assign({}, state, {
-      [action.species]: null
+      [action.species]: {data: null}
     });
   }
   if (action.type === FETCH_PET_SUCCESS) {
     return Object.assign({}, state, {
-      [action.species]: action.pet
+      [action.species]: {data: action.pet}
     });
   }
 
