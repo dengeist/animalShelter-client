10a35fb18b846e649c7354d4c27e947efe689afa
Elias Mason <eliasjmason@gmail.com>
2017-11-17 13:33:07 -0800

    Remove unnecessary variable

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 export const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||'http://localhost:8080/api';
